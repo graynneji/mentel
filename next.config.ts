4e7aaa773c51afdcf4108e66ab1f791eb61be524
Ukaegbu Gray Nneji <graynneji@outlook.com>
2026-03-19 13:59:21 -0700

    google search console error fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "index, follow",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
